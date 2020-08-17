7b57da6a9cb4585815641a8e00b8a7fb48b3ff27
Payton Gray <paytongray18@gmail.com>
2020-08-17 15:38:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
- planeteer_calls.map{|string|string.uppercase"!"}
+ planeteer_calls.map{|string|string.upcase"!"}
 end
 
 def long_planeteer_calls(planeteer_calls)
