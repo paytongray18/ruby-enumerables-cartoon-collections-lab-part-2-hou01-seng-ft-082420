9e8a9d2a080d9a31659d8f981808949ac12ff322
Payton Gray <paytongray18@gmail.com>
2020-08-17 15:50:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? {|x| x.length
+  planeteer_calls.any? {|x| x.length > 4}
 end
 
 def find_valid_calls(planeteer_calls)
