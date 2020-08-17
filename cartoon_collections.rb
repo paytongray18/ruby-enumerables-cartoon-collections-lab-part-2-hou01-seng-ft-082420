b757ab2cd0c29f56c310a20d60ac38d93ac9503b
Payton Gray <paytongray18@gmail.com>
2020-08-17 16:20:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find {valid_calls.include?}
+  planeteer_calls.find {|string| valid_calls.include?}
 end
