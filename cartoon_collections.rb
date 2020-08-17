3919b66cc6deef59b5de117a904930f1c628b567
Payton Gray <paytongray18@gmail.com>
2020-08-17 15:42:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,6 +7,7 @@ end
 def summon_captain_planet(planeteer_calls)
  planeteer_calls.map do |planeteer_calls|
    puts "#{planeteer_calls.capitalize}!"
+ end
 end
 
 def long_planeteer_calls(planeteer_calls)
